52053d8ad99ede9bd5e499114d1660fbcc02824f
Clarence Anne <burgosclarens@gmail.com>
2016-12-28 15:47:07 +0800

    Outputting posts stored in the database using a loop including the user's name and the time it is created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,30 +17,20 @@
 	<section class="row posts">
 		<div class="col-md-6 col-md-offset-3">
 			<header><h3>What other people say...</h3></header>
-			<article class="post">
-				<p>Asffghjklqwertyuiopzxcvbnm...</p>
-				<div class="info">
-					Posted by Clarens on 28 December 2016 
-				</div>
-				<div class="interaction">
-					<a href="#">Like</a>
-					<a href="#">Dislike</a>
-					<a href="#">Edit</a>
-					<a href="#">Delete</a>
-				</div>
-			</article>
-			<article class="post">
-				<p>Asffghjklqwertyuiopzxcvbnm...</p>
-				<div class="info">
-					Posted by Clarens on 28 December 2016 
-				</div>
-				<div class="interaction">
-					<a href="#">Like</a>
-					<a href="#">Dislike</a>
-					<a href="#">Edit</a>
-					<a href="#">Delete</a>
-				</div>
-			</article>
+			@foreach($posts as $post)
+				<article class="post">
+					<p>{{ $post->body }}</p>
+					<div class="info">
+						Posted by {{ $post->user->first_name }} on {{ $post->created_at }} 
+					</div>
+					<div class="interaction">
+						<a href="#">Like</a>
+						<a href="#">Dislike</a>
+						<a href="#">Edit</a>
+						<a href="#">Delete</a>
+					</div>
+				</article>
+			@endforeach
 		</div>
 	</section>
 @endsection
